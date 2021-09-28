2516e468dfca4523091be8b0395619f34fe6adc9
Jacko Jackonelli <jakob.lindqvist.92@gmail.com>
2021-09-28 17:33:22 +0200

    Removed dublettes from Home- /App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onMounted } from "vue";
-import { useStore } from "@/store";
-import { ActionTypes } from "@/store/actions";
-import { StoreApi } from "@/store/api";
+import { defineComponent } from "vue";
 import { eventbus } from "@/eventbus";
 import Footer from "@/components/layouts/Footer.vue";
 import Nav from "@/components/layouts/Nav.vue";
@@ -22,17 +19,7 @@ export default defineComponent({
   },
   setup() {
     eventbus.start();
-    const store = useStore();
-    const storeApi = new StoreApi();
-    const loading = computed(() => store.state.loading);
-    const natsClientStatus = computed(() => storeApi.getNatsClientStatus());
-    onMounted(() => {
-      storeApi.fauxLoading();
-    });
-    const mashTemp = computed(() => storeApi.getSensorValue("mash_temp"));
-    const boilTemp = computed(() => storeApi.getSensorValue("boil_temp"));
-    const activeSensors = computed(() => Array.from(storeApi.sensorClients()));
-    return { loading, mashTemp, boilTemp, natsClientStatus, activeSensors };
+    return {};
   },
 });
 </script>
