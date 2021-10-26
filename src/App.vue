d32cbde4228d1452f452cc6218f593de9a6f01df
Jakob <jakob.lindqvist@chalmers.se>
2021-10-26 13:09:40 +0200

    Spring cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
   <div class="flex flex-col justify-between h-screen">
-    <Nav />
     <router-view />
-    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { eventbus } from "@/eventbus";
-import Footer from "@/components/layouts/Footer.vue";
-import Nav from "@/components/layouts/Nav.vue";
 
 export default defineComponent({
-  components: {
-    Footer,
-    Nav,
-  },
+  components: {},
   setup() {
     eventbus.start();
     return {};
