d22f5b992d1d8b644087a20ffaea756d1ea13cbb
Jacko Jackonelli <jakob.lindqvist.92@gmail.com>
2021-11-15 20:29:24 +0100

    Track man/auto state

--- src/nats_setup.ts
+++ src/nats_setup.ts
@@ -1,5 +1,6 @@
 export const natsSettings = {
-  servers: "ws://localhost:9222",
+  // servers: "ws://localhost:9222",
+  servers: "ws://192.168.1.2:9222",
   user: "jackonelli",
   pass: "penna1glas",
 };
