d9881074f03caf89aa6fd7a641e29164b02c3d95
Jacko Jackonelli <jakob.lindqvist.92@gmail.com>
2024-03-14 07:30:57 +0100

    Cleaned up API

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,6 @@
-import { ControllerProps, Target } from "@/models/controller";
 import { eventbus } from "@/eventbus";
 import { useContrStore } from "@/stores/controller";
-import { useActorsStore } from "@/stores/actor";
-import { useSensorsStore } from "@/stores/sensor";
+import { ControllerProps, Target } from "@/models/controller";
 
 export class StoreApi {
   public async startController(
@@ -14,9 +12,8 @@ export class StoreApi {
 
   public async stopController(props: ControllerProps): Promise<void> {
     await eventbus.stopController(props);
-
-    const storeP = useContrStore();
-    storeP.removeClient(props.controllerId);
+    const store = useContrStore();
+    store.removeClient(props.controllerId);
   }
 
   public async switchController(
@@ -29,25 +26,4 @@ export class StoreApi {
   public setContrTarget(id: string, newTarget: Target): void {
     eventbus.setTarget(id, newTarget);
   }
-
-  public getActiveClients(): ActiveClients {
-    const contrStore = useContrStore();
-    const sensorStore = useSensorsStore();
-    const actorStore = useActorsStore();
-    const controllers = contrStore.clients;
-    const sensors = sensorStore.clients;
-    const actors = actorStore.clients;
-    const clients: ActiveClients = {
-      controllers,
-      sensors,
-      actors,
-    };
-    return clients;
-  }
-}
-
-interface ActiveClients {
-  controllers: IterableIterator<string>;
-  sensors: IterableIterator<string>;
-  actors: IterableIterator<string>;
 }
